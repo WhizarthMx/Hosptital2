175951c071756e63da8f3ababde9259c8b1b27d6
Juliooo <juliogrupo935@gmail.com>
2018-12-09 01:44:50 -0800

    Blade

--- public/index.php
+++ public/index.php
@@ -56,7 +56,7 @@ $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 //);
 
 $response = $kernel->handle(
-    $request = app\Custom\Http\Request::capture()
+    $request = App\Custom\Http\Request::capture()
 );
 
 $response->send();
