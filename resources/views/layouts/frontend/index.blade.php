79e74bc24372b767921a45e0c2cac4f6440c510f
Juliooo <juliogrupo935@gmail.com>
2018-12-09 01:21:15 -0800

    Blade

--- resources/views/layouts/frontend/index.blade.php
+++ resources/views/layouts/frontend/index.blade.php
@@ -7,10 +7,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet"/>
-    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet"/>
-    <link href="{{asset('css/main_styles.css')}}" rel="stylesheet"/>
-    <link href="{{asset('css/responsive.css')}}" rel="stylesheet"/>
+    <link href="{{HTML::style('css/bootstrap.min.css',array(),true)}}" rel="stylesheet"/>
+    <link href="{{HTML::style('css/font-awesome.min.css',array(),true)}}" rel="stylesheet"/>
+    <link href="{{HTML::style('css/main_styles.css',array(),true)}}" rel="stylesheet"/>
+    <link href="{{HTML::style('css/responsive.css',array(),true)}}" rel="stylesheet"/>
+
 
 
 
